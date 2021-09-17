38e5699f08a40428cf59099f0271a253e10d717f
Kevin Evans <k.evans.c@gmail.com>
2021-09-17 16:48:24 +0200

    about page animation work. about title animation behaves weird due to overlapping kinetic container

--- src/components/BaseAbout.vue
+++ src/components/BaseAbout.vue
@@ -1,48 +1,103 @@
 <template>
-    <div class="container baseAboutRoot">
+    <kinesis-container :duration="500" :perspective="1000000" :active="true" class="container baseAboutRoot">
         <div class="row">
-            <div class="col-sm-12">
-                <div class="">
-                    <div class="aboutTitleWrapper">
-                        <h2 class="aboutTitle aboutTitleBack">Who I Am.</h2>
-                        <h2 class="aboutTitle aboutTitleMid">Who I Am.</h2>
-                        <h2 class="aboutTitle aboutTitleFront util-outline-red">Who I Am.</h2>
-                    </div>
-                </div>
+            <div class="col-sm-10 offset-sm-1">
+                <h2 class="aboutTitleWrapper">
+                    <kinesis-element class="aboutTitle aboutTitleBack"
+                        type="depth_inv"
+                        :strength="-5"
+                        :originX="60"
+                        :originY="10"
+                        :minX="-3"
+                        :maxY="5"
+                        transformOrigin="100% 100% -6cm"
+                    >
+                        Who I Am.
+                    </kinesis-element>
+                    <kinesis-element class="aboutTitle aboutTitleMid"
+                        type="depth_inv"
+                        :strength="-10"
+                        :originX="60"
+                        :originY="10"
+                        :minX="-3"
+                        :maxY="10"
+                        transformOrigin="100% 100% 2cm"
+                    >
+                        Who I Am.
+                    </kinesis-element>
+                    <kinesis-element class="aboutTitle aboutTitleFront util-outline-red"
+                        type="depth_inv"
+                        :strength="-10"
+                        :originX="60"
+                        :originY="10"
+                        :minX="-3"
+                        :maxY="10"
+                        transformOrigin="100% 100% 9cm"
+                    >
+                        Who I Am.
+                    </kinesis-element>
+                </h2>
             </div>
         </div>
-        <div class="row">
-            <div class="col-sm-6">
+        <kinesis-container :duration="300" event="scroll" class="row testtt">
+            <div class="col-sm-5 offset-sm-1">
                 <div class="aboutLeft">
-                    <p class="aboutText">
+                    <kinesis-element 
+                        tag="p" 
+                        class="aboutText"
+                        :strength="-80">
                         I am a frontend developer with passion for interface design and 
                         unique user experiences on the web. I got an engineering 
                         background in media technology where I’ve been working with 
                         everything from sound processing to computer graphics to interaction design.
-                    </p>
-                    <div class="aboutTextRect"></div>
+                    </kinesis-element>
+                    <kinesis-element 
+                        class="aboutTextRect"
+                        type="translate"
+                        :strength="140"
+                        :maxY="20">
+                    </kinesis-element>
                 </div>
             </div>
-            <div class="col-sm-4">
+            <div class="col-sm-4 offset-sm-1">
                 <div class="aboutRight">
-                    <p class="aboutText">
+                    <kinesis-element tag="img" :strength="80" class="aboutPortrait behind" src="/porttrait-fill.png" alt="portrait image of Kevin Evans" />
+                    <kinesis-element tag="img" :strength="-80" class="aboutPortrait" src="/porttrait.png" alt="portrait image of Kevin Evans" />
+                    <kinesis-element 
+                        tag="p" 
+                        class="aboutText"
+                        :strength="-280"
+                        :maxY="-90">
                         I love being creative with photography, music, graphic design, 
                         clothing and painting but most of all digital experiences. 
                         Looking at good content and using great products inspires me 
                         everytime I stumble upon them. With this drive I aim to 
                         create experiences people remember and sincerly enjoy.
-                    </p>
-                    <div class="aboutTextRect"></div>
+                    </kinesis-element>
+                    <kinesis-element 
+                        class="aboutTextRect"
+                        :strength="20"></kinesis-element>
                 </div>
             </div>
+        </kinesis-container>
+        <div class="row">
+            <div class="col-sm-6">
+                <h3>Socials / Creative Outlets</h3>
+            </div>
         </div>
-    </div>
+    </kinesis-container>
 </template>
 
 <script>
+import { KinesisContainer, KinesisElement } from "vue-kinesis";
+
 
 export default {
     name: "BaseAbout",
+    components: {
+        KinesisElement,
+        KinesisContainer
+    },
     data () {
         return {
 
@@ -63,12 +118,14 @@ export default {
 <style lang="scss">
 .baseAboutRoot{
     background-color: var(--about-bg);
-    padding-bottom: 190px;
+    padding-top: 30px;
+    padding-bottom: 90px;
+
+    //.testtt{background-color: green;}
 
     .aboutTitleWrapper{
-        padding-top: 60px;
-        padding-left: 40px;
-        padding-bottom: 50px;
+        margin-top: 80px;
+        margin-bottom: 280px;
         position: relative;
         z-index: 1;
 
@@ -80,18 +137,18 @@ export default {
 
         .aboutTitleBack{
             position: absolute;
-            left: 0px;
-            top: 40px;
+            //left: -25px;
+            //top: -15px;
             color: #2e2e2e;
             z-index: -1;
         }
-        .aboutTitleMiddle{
-            
+        .aboutTitleMid{
+            position: absolute;
         }
         .aboutTitleFront{
             position: absolute;
-            left: 100px;
-            bottom: 10px;  
+            //left: 50px;
+            //top: 10px;  
             color: transparent; 
             z-index: 1;
         }
@@ -99,15 +156,53 @@ export default {
 
     .aboutLeft, .aboutRight{
         padding-top: 50px;
+        position: relative;
+        z-index: 1;
 
         .aboutText{
             color: white;
+            font-size: 16px;
+            line-height: 30px;
+            margin-top: -30px;
         }
 
+        .aboutTextRect{
+            display: block;
+            position: absolute;
+            top: 30px;
+            left: -20px;
+            z-index: -1;
+            width: 40%;
+            height: 70%;
+            background-color: var(--applered);
+        }
     }
 
     .aboutLeft{
-        
+
+        .aboutTextRect{
+            width: 60%;
+            height: 70%;
+        }
+    }
+    .aboutRight{
+        margin-top: 40px;
+        z-index: 10;
+
+        .aboutPortrait{
+            position: absolute;
+            z-index: -1;
+            left: 30px;
+            width: 55%;
+            top: -280px;
+            height: auto;
+            object-fit: fill;
+        }
+        .aboutPortrait.behind{
+            left: 0px;
+            top: -300px;
+        }
+
     }
 }
 
